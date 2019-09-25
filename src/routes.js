8dce116235cccbd9a871473e33b7331ffb26b2cc
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2019-09-24 23:40:43 -0300

    feature: set appointments

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 // Middlewares
 import authMiddleware from './app/middlewares/auth';
@@ -19,13 +20,19 @@ const upload = multer(multerConfig);
 routes.use(access);
 // Routes
 routes.post('/users', UserController.store);
+
 routes.post('/sessions', SessionController.store);
+
 routes.get('/users/:email', UserController.index);
 
 // Routes from now on, will use this middleware(global from this point)
 routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
+routes.post('/appointments', AppointmentController.store);
 export default routes;
