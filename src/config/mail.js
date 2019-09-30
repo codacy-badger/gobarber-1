b46541f2f6ee64156f6bb757284081bb40ed9ef5
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2019-09-30 12:26:47 -0300

    chore: nodemailer added

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '8f5e925cf28473',
+    pass: '12fb15fcfa784e',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
