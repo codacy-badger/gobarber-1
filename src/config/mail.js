f66b982525b6e3a61b9f78b91d85fb2b67bbcfa1
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2019-09-30 12:29:28 -0300

    chore: nodemailer added

--- src/config/mail.js
+++ src/config/mail.js
@@ -3,8 +3,8 @@ export default {
   port: '2525',
   secure: false,
   auth: {
-    user: '',
-    pass: '',
+    user: '8f5e925cf28473',
+    pass: '12fb15fcfa784e',
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
