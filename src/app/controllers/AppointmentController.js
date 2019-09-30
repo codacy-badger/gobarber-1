a9508e7eec09be7c13bb32cb217016bef3435a65
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2019-09-30 13:04:04 -0300

    feature: email provider when appointment canceled

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,6 +1,7 @@
 import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
+import Mail from '../../lib/Mail';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
@@ -117,7 +118,15 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
     if (appointment.user_id !== req.userId) {
       return res
         .status(401)
@@ -135,6 +144,12 @@ class AppointmentController {
     appointment.canceled_at = new Date();
 
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento Cancelado',
+      text: 'Você tem um novo cancelamento',
+    });
 
     return res.json(appointment);
   }
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,6 +1,6 @@
 export default {
   host: 'smtp.mailtrap.io',
-  port: '2525',
+  port: 2525,
   secure: false,
   auth: {
     user: '8f5e925cf28473',
