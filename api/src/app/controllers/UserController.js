55639cdb248093af96576628093ec21cba58a4e7
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2020-03-30 14:11:53 -0300

    Dependencies upgraded

--- api/src/app/controllers/UserController.js
+++ api/src/app/controllers/UserController.js
@@ -54,7 +54,7 @@ class UserController {
     const { email, oldPassword } = req.body;
     const user = await User.findByPk(req.userId);
 
-    if (email !== user.email) {
+    if (email && email !== user.email) {
       if (await User.findOne({ where: { email } })) {
         return res.status(400).json({ error: 'User already exists' });
       }
